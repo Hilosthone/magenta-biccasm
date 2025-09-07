5cdfc7a06456e476d1b94af7a34f9ec3168c8910
Hilosthone <solihullahsulyman@gmail.com>
2025-09-07 03:31:13 +0100

    Describe what you updated (e.g. fixed navbar, added gradient)

--- vite.config.ts
+++ vite.config.ts
@@ -6,31 +6,4 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/magenta-biccasm/',
-  build: {
-    // 1. Optional: raise warning limit if your app is OK being larger
-    chunkSizeWarningLimit: 1000, // kB
-
-    // 2. Optional: tell Rollup how to split large dependencies
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          // Example: put React-related libs in one chunk
-          react: ['react', 'react-dom'],
-          // Example: split charting libs or other heavy deps
-          // charting: ['chart.js', 'd3'],
-        },
-      },
-    },
-  },
 })
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   base: '/magenta-biccasm/',
-// })
